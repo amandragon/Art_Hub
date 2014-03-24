1d4047930395bdf63f29f730feacf2bc207b2601
amandragon <aee8@duke.edu>
2014-03-24 13:05:55 -0700

    relationships

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ActiveRecord::Base
   has_many :reviews
+
+    def self.all_posts_by user
+    where(user_id: user.id)
+    end
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,9 @@
 class Review < ActiveRecord::Base
   belongs_to :user, through :post
+
+   def self.all_reviews_by user
+    where(user_id: user.id)
+    # where review user_id is equal to the user's ID
+    end
+
 end
