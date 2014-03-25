e4d3ea8994414ca7183bbff650394ef7e4d50bd2
amandragon <aee8@duke.edu>
2014-03-25 11:17:22 -0700

    ajax

--- app/assets/javascripts/review.js
+++ app/assets/javascripts/review.js
@@ -2,6 +2,15 @@
 
 $(document).ready(function() {
 
+  console.log('READY!');
+  $('#new_review').append('TESTING, that is the form ...');
+
+  $('#new_review').on("ajax:success", function(e,data,status, xhr){
+    console.log('Ajax is back');
+    // $('#new_review').append(xhr.responseText);
+  }).bind("ajax:error", function(e,data,status, xhr){
+    console.log('Ajax error');
+  });
 
 // jquery
     $('button').click(function() {
@@ -9,10 +18,16 @@ $(document).ready(function() {
         $('#messages').append("<p>"+toAdd+"</p>");
     });
 
+
+
+  // $('submit').click(function(){
 // turn this into a form for a review with the following:
 // post_id, title, text, score, user_id
 
 // make an ajax call
+
+
+  // });
 
 
 });
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -14,7 +14,11 @@ class ReviewsController < ApplicationController
     new_review=params.require(:review).permit( :title, :text, :score, :user_id, :post_id)
     @review = Review.create(new_review)
     @review.update_attributes(user_id: current_user.id)
-    render :show
+    
+    respond_to do |format| 
+      format.json { render :json => @post }
+    end  
+
   end
   
   def show
