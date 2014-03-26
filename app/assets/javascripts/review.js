7ddb67d6f2752767266472ba672e5b456477f44b
amandragon <aee8@duke.edu>
2014-03-26 16:28:45 -0700

    javascript is working

--- app/assets/javascripts/review.js
+++ app/assets/javascripts/review.js
@@ -1,13 +1,25 @@
 // put the new review partial here
 
 $(document).ready(function() {
-
   console.log('READY!');
-  $('#new_review').append('TESTING, that is the form ...');
 
   $('#new_review').on("ajax:success", function(e,data,status, xhr){
-    $('#new_review').append(xhr.responseText);
-  }).bind("ajax:error", function(e,data,status,3 xhr){
+    review = jQuery.parseJSON(xhr.responseText);
+    console.log(review);
+    var lastElem = $('#review_list').children().last();
+    var new_review_html = "<div class='row'><div class='span3 offset1'><dl><dt>" + review["title"] + " (" + review["score"] + ")</dt><dd>"+review["text"]+"</dd></dl></div></div>";
+    $(new_review_html).appendTo(lastElem);
+  
+    // <div class="row">
+    //   <div class="span3 offset1">
+    //     <dl>
+    //     <dt><%=review.title%> (<%=review.score%>)</dt>
+    //     <dd><%=review.text%></dd>
+    //     </dl>
+    //   </div>
+    // </div>
+
+  }).bind("ajax:error", function(e,data,status, xhr){
     console.log('Ajax error');
   });
 
