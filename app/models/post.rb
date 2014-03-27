17b872c2f304e3ac2995ed9d621255f755f43b48
amandragon <aee8@duke.edu>
2014-03-26 17:04:13 -0700

    validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ActiveRecord::Base
   has_many :reviews
 
+  validates :user_id, presence: true
+
   def self.all_posts_by user
     where(user_id: user.id)
   end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,8 @@
 class Review < ActiveRecord::Base
   belongs_to :user
 
+  validates :post_id, presence: true
+
   def self.all_reviews_by user
     where(user_id: user.id)
     # where review user_id is equal to the user's ID
